5d57895fd502e729fb1d8370dd9e6bd18e032ee3
liu fengyun <liu@fengy.me>
2016-11-24 10:24:20 +0100

    walkaround junit problem

--- macros/src/test/scala/MacrosTest.scala
+++ macros/src/test/scala/MacrosTest.scala
@@ -1,20 +1,27 @@
 import org.junit.Test
 import org.junit.Assert._
 
-class MacrosTest {
+object MacrosTest {
+  def main(args: Array[String]): Unit = {
+    helloObject
+    helloDef
+    helloVal
+    println("========= All tests pass ==========")
+  }
+
   @Test def helloObject = {
     @hello object Foo
-    assertTrue(Hello.hello == 1024)
+    assert(Hello.hello == 1024)
   }
 
   @Test def helloDef = {
     @helloDef def hello = "hello"
-    assertTrue(hello == 1024)
+    assert(hello == 1024)
   }
 
   @Test def helloVal = {
     @helloVal val hello = "hello"
-    assertTrue(hello == 1024)
+    assert(hello == 1024)
   }
 }
 
