55da943f2c2edf76702423aad65dc3a4720f50ec
liu fengyun <liu@fengy.me>
2016-11-30 02:32:54 +0100

    test framework support multiple suite

--- macros/src/test/scala/Driver.scala
+++ macros/src/test/scala/Driver.scala
@@ -1,6 +1,9 @@
-object Driver {
-  val tests = new collection.mutable.HashMap[String, () => Unit]()
+import scala.collection.mutable
 
+object Driver {
+  type Test = () => Unit
+  type Suite = collection.mutable.HashMap[String, () => Unit]
+  val suites = new collection.mutable.HashMap[String, Suite]
 
   def main(args: Array[String]): Unit = {
     // setup tests
@@ -8,30 +11,50 @@ object Driver {
     new QuasiquoteSuite
 
     var success = false
-    var failed = 0
-    tests.foreach { case (name, code) =>
-      try { code(); success = true }
-      catch {
-        case ex: Throwable =>
-          success = false
-          failed += 1
-          print(Console.WHITE)
-          ex.printStackTrace()
+    var total = 0
+    var totalFailed = 0
+    suites.foreach { case (name, tests) =>
+      var failed = 0
+      println(s"\n\n======= $name =========")
+      tests.foreach { case (name, code) =>
+        try {
+          code(); success = true
+        }
+        catch {
+          case ex: Throwable =>
+            success = false
+            failed += 1
+            print(Console.WHITE)
+            ex.printStackTrace()
+        }
+
+        if (success)
+          println(s"${Console.GREEN}- $name")
+        else
+          println(s"${Console.RED}- $name")
       }
 
-      if (success)
-        println(s"${Console.GREEN}- $name")
-      else
-        println(s"${Console.RED}- $name")
+      total += tests.size
+      totalFailed += failed
+
+      println(s"${Console.WHITE}Tests: ${tests.size}, Success: ${tests.size - failed}, Failed: $failed")
     }
 
-    println(s"${Console.WHITE}Total: ${tests.size}, Success: ${tests.size - failed}, Failed: $failed")
+    println(s"\n\n [${suites.size} Suites] ${Console.WHITE}Tests: $total, Success: ${total - totalFailed}, Failed: $totalFailed")
   }
 }
 
 class TestSuite {
   def test(name: String)(code: => Unit): Unit = {
-    assert(!Driver.tests.contains(name))
-    Driver.tests.update(name, () => code)
+
+    val suiteName = this.getClass.getName
+    val suite = if (!Driver.suites.contains(suiteName)) {
+      val s = new Driver.Suite()
+      Driver.suites += suiteName -> s
+      s
+    } else Driver.suites(suiteName)
+
+    assert(!suite.contains(name))
+    suite.update(name, () => code)
   }
 }
