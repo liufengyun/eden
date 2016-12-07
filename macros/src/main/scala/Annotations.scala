66e6955996eb26aa6ba166bbd984e1e8626d90a9
liu fengyun <liu@fengy.me>
2016-12-07 17:39:19 +0100

    add test for @data

--- macros/src/main/scala/Annotations.scala
+++ macros/src/main/scala/Annotations.scala
@@ -1,4 +1,5 @@
 import scala.`meta`._
+import scala.`meta`.dialects.Dotty
 
 class main extends scala.annotation.MacrosAnnotation {
   def apply(defn: Any): Any = meta {
@@ -7,6 +8,25 @@ class main extends scala.annotation.MacrosAnnotation {
       def stub(args: Any): Any = { ..$stats }
     """
     q"object $name { $main }"
+  }
+}
+
+class data extends scala.annotation.MacrosAnnotation  {
+  def apply(defn: Any): Any = meta {
+    defn match {
+      case q"class $name(..$params)" =>
+        val params1 = params.map(_.copy(mods = List(mod"valparam")))
+        val class1 = q"class $name(..$params1)"
+
+        val applyParams = params.map(_.copy(mods = Nil))
+        val applyArgs = params.map(p => Term.Name(p.name.syntax))
+        val apply = q"def apply(..$applyParams) = new ${Ctor.Name(name.syntax)}(..$applyArgs)"
+        val object1 = q"object ${Term.Name(name.syntax)} { $apply }"
+
+        q"$class1; $object1"
+      case _ =>
+        abort("@data can only annotate classes")
+    }
   }
 }
 
--- macros/src/test/scala/MacrosTest.scala
+++ macros/src/test/scala/MacrosTest.scala
@@ -6,4 +6,11 @@ class MacrosTest extends TestSuite {
 
     assert(Test.stub(null) == "hello world!")
   }
+
+  test("data") {
+    @data class Point(x: Int, y: Int)
+
+    val p = Point(40, 2)
+    assert(p.x == 40 && p.y == 2)
+  }
 }
