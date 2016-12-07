bc7a6ec8927d46af2c203205d5726a6063c18705
liu fengyun <liu@fengy.me>
2016-12-07 13:18:17 +0100

    remove quasiquote tests from macros

--- macros/src/test/scala/MacrosTest.scala
+++ macros/src/test/scala/MacrosTest.scala
@@ -1,39 +1,4 @@
 class MacrosTest extends TestSuite {
-  test("helloObject") {
-    @hello object Foo
-    assert(Hello.hello == "hello, world!")
-  }
-
-  test("helloDef") {
-    @helloDef def hello = "hello"
-    assert(hello == "hello, world!")
-  }
-
-  test("helloVal") {
-    @helloVal val hello = "hello"
-    assert(hello == 1024 + 256)
-  }
-
-  test("helloNested") {
-    @helloNested val hello = "hello"
-    assert(hello == 1024)
-  }
-
-  test("helloTrees") {
-    @helloTrees val hello = "hello"
-    assert(hello == 1024)
-  }
-
-  test("helloQuasiInSeq") {
-    @helloQuasiInSeq val hello = "hello"
-    assert(hello == 1024)
-  }
-
-  test("helloRank2") {
-    @helloRank2 val hello = "hello"
-    assert(hello == 1024)
-  }
-
   test("main") {
     @main object Test {
       "hello world!"
